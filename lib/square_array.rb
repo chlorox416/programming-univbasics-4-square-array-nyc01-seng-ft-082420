0299023c0befb588c2ceee60316e07265624f717
C. Chang <chloechang416@gmail.com>
2020-08-14 01:34:49 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -18,8 +18,8 @@
 #   end
 # end
 
-# def square_array(array)
-#   squared = []
+def square_array(array)
+  squared = []
   count = 0
   while count < array.size do
     squared << (array[count] ** 2)
